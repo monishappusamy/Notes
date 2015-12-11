1eb2b7f194f51ab828a8fd24fa26c97e810796ad
monish appusamy <monishappusamy@gmail.com>
2015-12-11 02:58:14 -0500

    Update TripAdvisor.py

--- Web-Analytics/TripAdvisor.py
+++ Web-Analytics/TripAdvisor.py
@@ -9,24 +9,30 @@ import re
 
 i=7
 n=1000
-while i<=n:
-    url = "http://www.tripadvisor.com/ShowUserReviews-g34438-d87137-r331479907-The_Mutiny_Hotel-Miami_Florida.html#or%i" %i
-    fileName = "page%i.html" % i
-    fw = open(fileName,'w')
-    driver = webdriver.Chrome('./chromedriver')
-    driver.get(url)
-    html = driver.page_source
-    for line in html:
-        try:
-            fw.write(line)
-        except:
-            #print 'exception'
-            continue
-    print 'page %i done' %i
-    if n == 1000:
-        totals=re.finditer('<a class="more taLnk" href="#REVIEWS">(.*?) Reviews</a>',html)    
-        for total in totals:
-            n = int(total.group(1).strip()) - 6
-    i += 6
-    driver.close()
-    fw.close()
+fileReader = open("haveto.txt")
+
+for line in fileReader:
+    line = line.strip()
+    hotelName, url = line.split('\t')
+
+    while i<=n:
+        url = url + str(i)
+        filename = "page%i.html" % i
+        filename = "/Users/Monish/Desktop/660/Extra_Project/" + hotelName + "/" + filename
+        fw = open(filename,'w')
+        driver = webdriver.Chrome('./chromedriver')
+        driver.get(url)
+        html = driver.page_source
+        for line in html:
+            try:
+                fw.write(line)
+            except:
+                continue
+        print 'page %i done' %i
+        if n == 1000:
+            totals=re.finditer('<a class="more taLnk" href="#REVIEWS">(.*?) Reviews</a>',html)    
+            for total in totals:
+                n = int(total.group(1).strip()) - 6
+        i += 6
+        driver.close()
+        fw.close()
